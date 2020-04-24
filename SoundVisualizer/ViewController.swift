8a3c297b6193b723bc99847787ef2c3ecdb3ae24
hongjuyeon <coke1317@naver.com>
2020-04-24 09:23:05 +0900

    change deployment target, modify UI

--- SoundVisualizer/ViewController.swift
+++ SoundVisualizer/ViewController.swift
@@ -5,14 +5,16 @@
 //  Created by hongjuyeon_dev on 2020/04/23.
 //  Copyright © 2020 hongjuyeon_dev. All rights reserved.
 //
+// ref: https://medium.com/swlh/swiftui-create-a-sound-visualizer-cadee0b6ad37
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var visualizeView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
